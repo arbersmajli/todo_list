2ff7308343f43383293e0af88b98c337c2e2980a
Arber <arber.smajli@he-arc.ch>
2020-03-22 11:42:26 +0100

    Suivi du tuto https://www.youtube.com/watch?v=J7d1iseTV68&feature=emb_title
    Minute N° 36:48

--- app/src/main/java/com/example/todo_list/MainActivity.java
+++ app/src/main/java/com/example/todo_list/MainActivity.java
@@ -1,18 +1,101 @@
 package com.example.todo_list;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    final List<String> list = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView listView = findViewById(R.id.listView);
+        final TextAdapter adapter = new TextAdapter();
+
+        adapter.setData(list);
+        listView.setAdapter(adapter);
+
+        final Button newTaskButton = findViewById(R.id.newTaskButton);
+
+        newTaskButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                final EditText taskInput = new EditText(MainActivity.this);
+                taskInput.setSingleLine();
+                AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Ajouter une nouvelle tâche")
+                        .setMessage("Quelle est vôtre nouvelle tâche ?")
+                        .setView(taskInput)
+                        .setPositiveButton("Ajouter la tâche", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                list.add(taskInput.getText().toString());
+                                adapter.setData(list);
+                            }
+                        })
+                        .setNegativeButton("Annuler", null)
+                        .create();
+                dialog.show();
+            }
+        });
+    }
+
+    class TextAdapter extends BaseAdapter{
+
+        List<String> list = new ArrayList<>();
+
+        void setData(List<String> mList){
+            list.clear();
+            list.addAll(mList);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount(){
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position){
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position){
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.item, parent,false);
+            TextView textView = findViewById(R.id.task);
+            textView.setText(list.get(position));
+            return rowView;
+        }
+
+
+
     }
 
 }
